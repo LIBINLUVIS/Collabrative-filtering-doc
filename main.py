2b2a106f7bae43fb31a3dedf64115a06f178d25e
LIBINLUVIS <libinluvis10@gmail.com>
2022-01-22 13:05:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ def recommentation(request,pk):
         a=[item.user_id,item.isbn,item.rating]
         b+=[a]
     ratings_df=pd.DataFrame(b,columns=['user_id','ISBN','rating'])
-    x = ratings_df['user_id'].value_counts() > 1
+    x = ratings_df['user_id'].value_counts() > 200
     y = x[x].index
     ratings = ratings_df[ratings_df['user_id'].isin(y)]
     rating_with_books = ratings.merge(books_df, on='ISBN')
     number_rating = rating_with_books.groupby('title')['rating'].count().reset_index()
     number_rating.rename(columns= {'rating':'number_of_ratings'}, inplace=True)
     final_rating = rating_with_books.merge(number_rating, on='title')
-    final_rating = final_rating[final_rating['number_of_ratings'] >= 1]
+    final_rating = final_rating[final_rating['number_of_ratings'] >= 50]
     final_rating.drop_duplicates(['user_id','title'], inplace=True)
     book_pivot = final_rating.pivot_table(columns='user_id', index='title', values="rating")
     book_pivot.fillna(0, inplace=True)
